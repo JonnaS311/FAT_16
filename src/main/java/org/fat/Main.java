fa901ba65f6c4472000e2de98749c46bba26b150
CristianAgudeloB <163355725+CristianAgudeloB@users.noreply.github.com>
2024-06-07 22:44:44 -0500

    Añadido JavaFX

--- src/main/java/org/fat/Main.java
+++ src/main/java/org/fat/Main.java
@@ -1,10 +1,24 @@
 package org.fat;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import java.util.Arrays;
 
-public class Main {
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Configuracion.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
+        stage.setTitle("Simulador FAT-16");
+        stage.setScene(scene);
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
 
         TablaFAT test = new TablaFAT(65524,64, 0.0);
 
@@ -21,5 +35,8 @@ public class Main {
             }
             System.out.println();
         }
+
+        launch();
+
     }
 }
