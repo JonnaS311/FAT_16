b9e00cb3e08a7b6a4fd237b33c0f2509b8c7b4d2
AnnVil505 <danna13villanueva@gmail.com>
2024-06-08 10:52:34 -0500

    Ingreso datos iniciales

--- src/main/java/org/fat/Main.java
+++ src/main/java/org/fat/Main.java
@@ -13,9 +13,8 @@ import java.util.Objects;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        System.out.println(Main.class.getResource("Configuracion.fxml"));
-        Parent fxmlLoader = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("View.fxml")));
-        Scene scene = new Scene(fxmlLoader, 800, 400);
+        Parent fxmlLoader = FXMLLoader.load(Objects.requireNonNull(Main.class.getResource("Configuracion.fxml")));
+        Scene scene = new Scene(fxmlLoader);
         stage.setTitle("Simulador FAT-16");
         stage.setScene(scene);
         stage.show();
@@ -23,15 +22,15 @@ public class Main extends Application {
 
     public static void main(String[] args) {
 
-        TablaFAT test = new TablaFAT(65524,64, 0.0);
 
+/*
         test.createFile(62*65524*512);
         test.createFile(160000);
         test.createFile(56);
         test.deleteFile(63477);
         System.out.println(Arrays.toString(test.getTable()));
         System.out.println(test.getTable_first_cluster());
-
+*/
 
 
         launch();
--- src/main/java/org/fat/MainController.java
+++ src/main/java/org/fat/MainController.java
@@ -1,12 +1,15 @@
 package org.fat;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -15,6 +18,15 @@ public class MainController {
     @FXML
     private Label Advertencia;
 
+    @FXML
+    private TextField txtnumSector;
+
+    @FXML
+    private TextField txtcluster;
+    @FXML
+    private TableView<SimpleIntegerProperty> tablaFATvisual;
+
+
 
 
     //Comportamiento de los Botones para la Navegación
@@ -22,17 +34,30 @@ public class MainController {
     @FXML
     protected void IniciarSimulacion(ActionEvent event) throws IOException {
 
+        String numSectorText = txtnumSector.getText();
+        String numClusterText = txtcluster.getText();
 
-        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
-        Scene scene = new Scene(root);
-        stage.setTitle("Simulador FAT16 - Tabla de Asignación de Archivos");
-        stage.setScene(scene);
-        stage.centerOnScreen();
-        stage.show();
+        try {
+            int numSector = Integer.parseInt(numSectorText);
+            int numCluster = Integer.parseInt(numClusterText);
 
-            Advertencia.setText("Error al iniciar");
+            if (numSector > 0 && numCluster > 0) {
 
+                Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+                Parent root = FXMLLoader.load(getClass().getResource("view.fxml"));
+                Scene scene = new Scene(root);
+                stage.setTitle("Simulador FAT16 - Tabla de Asignación de Archivos");
+                stage.setScene(scene);
+                stage.centerOnScreen();
+                stage.show();
+
+                Advertencia.setText("Error al iniciar");
+            } else {
+                mostrarAlerta("Por favor, ingresa números mayores a cero.");
+            }
+        } catch (NumberFormatException e) {
+            mostrarAlerta("Por favor, ingresa números válidos en ambos campos.");
+        }
     }
 
     @FXML
@@ -55,5 +80,13 @@ public class MainController {
         stage.setScene(scene);
         stage.show();
 
+    }
+
+    private void mostrarAlerta(String mensaje) {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error de validación");
+        alert.setHeaderText(null);
+        alert.setContentText(mensaje);
+        alert.showAndWait();
     }
 }
